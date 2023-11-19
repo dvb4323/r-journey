f41fe8d7bb780755bb0dbf2730849baa916f030f
Do Van Binh <111190439+dvb4323@users.noreply.github.com>
2023-11-19 21:52:07 +0700

    Add code

---
+++ DiamondAnalyzing.R
@@ -0,0 +1,11 @@
+
+
+mydata <- read.csv(file.choose())
+
+install.packages("ggplot2")
+
+library("ggplot2")
+
+ggplot(data=mydata[mydata$carat<2.5,], aes(x=carat, y=price, colour=clarity)) + 
+  geom_point(alpha = 0.1) +
+  geom_smooth()
